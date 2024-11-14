34c43e8a83644191eb525a6860ef4531de7a4785
Jabes Protazio <60739438+jabesprotazio@users.noreply.github.com>
2024-11-14 08:48:36 -0300

    New lessons and learn of paradigms  of oriented object!

--- src/academy/devdojo/maratonajava/javacore/Dconstrutores/test/AnimeTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Dconstrutores/test/AnimeTest01.java
@@ -4,7 +4,8 @@ import academy.devdojo.maratonajava.javacore.Dconstrutores.dominio.Anime;
 
 public class AnimeTest01 {
     public static void main(String[] args) {
-        Anime anime = new Anime("Akudama Drive","TV",12,"Ação");
+
+        Anime anime = new Anime("Akudama Drive","TV",12,"Ação", "Production IG");
         Anime anime2 = new Anime();
 
         anime.imprime();
