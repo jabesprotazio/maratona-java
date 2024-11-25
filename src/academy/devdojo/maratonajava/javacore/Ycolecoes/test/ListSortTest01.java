cd94bad36d35105a01d3306ba59cc638a5858fe7
Jabes Protazio <60739438+jabesprotazio@users.noreply.github.com>
2024-11-25 20:36:34 -0300

    The study of List show that the way of comparable two lists is through of Interface Comparable,then the methon compareTo() is implament in the class that implements the Interface Comparable, so the item(id) of comparation of object is used in methon for compare.
    Observation; if the item of this(Object) is less of item the object(parameter) so the return is -1, if equals so the retorn is 0, if not the return is 1.

---
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/ListSortTest01.java
@@ -0,0 +1,40 @@
+package academy.devdojo.maratonajava.javacore.Ycolecoes.test;
+
+import academy.devdojo.maratonajava.javacore.Ycolecoes.dominio.Manga;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ListSortTest01 {
+    public static void main(String[] args) {
+        List<String> mangas = new ArrayList<>(6);
+        mangas.add("Berserk");
+        mangas.add("Hellsing Ultimate");
+        mangas.add("Pokemon");
+        mangas.add("Attack on titan");
+        mangas.add("Dragon ball 2");
+
+        Collections.sort(mangas);
+
+        List<Double> dinheiros = new ArrayList<>();
+        dinheiros.add(100.21);
+        dinheiros.add(23D);
+        dinheiros.add(21.21);
+        dinheiros.add(98.10);
+
+
+        System.out.println(dinheiros);
+        Collections.sort(dinheiros);
+        System.out.println(dinheiros);
+
+
+
+        for (String manga : mangas){
+            System.out.println(manga);
+        }
+
+
+        new Manga(546847136L, null, 0);
+    }
+}
