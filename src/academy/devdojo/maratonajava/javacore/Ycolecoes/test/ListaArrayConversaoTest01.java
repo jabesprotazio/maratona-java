5ab309e0dbfa901e5e3867a6cdb7d98c2f9aaa81
Jabes Protazio <60739438+jabesprotazio@users.noreply.github.com>
2024-12-10 17:25:23 -0300

    In this case, is necessary transformation the List to Array, so you can use the methon toArray().For contrary, use the methon Arrays.asList().
    But, take care, because this methon(Arrays.asList()) call(conect) with Array, so when you modific the new List the Array also modific, or when you add an alement, an error is generated(Unsupported).
    
    So, what the batter way?
    You can use the constructor of ArrayList(overloaded,int initial or Colletion), in this case I used a Colletion(Array) into methon Arrays.asList().
    
    Attention, from Java 8 Language level, you can do ArrayList through Arrays.asList(content);

--- src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/ListaArrayConversaoTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/ListaArrayConversaoTest01.java
@@ -1,5 +1,41 @@
 package academy.devdojo.maratonajava.javacore.Ycolecoes.test;
 
-public class ListaArrayConversaoTest01 {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+public class ListaArrayConversaoTest01 {
+    public static void main(String[] args) {
+
+
+
+        List<Integer> numerosList = new ArrayList<>();
+        numerosList.add(1);
+        numerosList.add(2);
+        numerosList.add(3);
+
+        Integer[] listToArray = numerosList.toArray(new Integer[0]);
+        System.out.println(Arrays.toString(listToArray));
+        System.out.println("-------------");
+
+
+        Integer[] numerosArray = new Integer[3];
+        numerosArray[0] = 1;
+        numerosArray[1] = 2;
+        numerosArray[2] = 3;
+
+        /*List<Integer> arrayToList = Arrays.asList(numerosArray);
+        //arrayToList.set(1,12);
+        arrayToList.add(13);
+        System.out.println(Arrays.toString(numerosArray));
+        System.out.println(arrayToList);*/
+
+        System.out.println("----------------");
+        List<Integer> numerosLista = new ArrayList<>(Arrays.asList(numerosArray));
+        numerosLista.add(15);
+        System.out.println(numerosLista);
+
+        Arrays.asList(1,3);
+
+    }
 }
