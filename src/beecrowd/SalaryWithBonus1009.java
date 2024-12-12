f8641b31d6bb1036c48b33b5641a8f094d15ee8b
Jabes Protazio <60739438+jabesprotazio@users.noreply.github.com>
2024-12-12 11:09:34 -0300

    Make a program that reads a seller's name, his/her fixed salary and the sale's total made by himself/herself in the month (in money). Considering that this seller receives 15% over all products sold, write the final salary (total) of this seller at the end of the month , with two decimal places.

---
+++ src/beecrowd/SalaryWithBonus1009.java
@@ -0,0 +1,20 @@
+package beecrowd;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class SalaryWithBonus1009 {
+    public static void main(String[] args) {
+        String name;
+        double porcentagem = 15;
+        double salaryFix, sales, total;
+        Scanner keyboard = new Scanner(System.in);
+        DecimalFormat df = new DecimalFormat("0.00");
+        name = keyboard.next();
+        salaryFix = keyboard.nextDouble();
+        sales = keyboard.nextDouble();
+
+        total = (sales *  (porcentagem/100)) + salaryFix;
+        System.out.println("TOTAL = R$ " + df.format(total));
+    }
+}
