5c251c573afd40eaecb56dcfe31a52720f1cd77f
Jabes Protazio <60739438+jabesprotazio@users.noreply.github.com>
2024-12-29 00:20:51 -0300

    This case is very interesting in my opinion, because I remember of case of Arry multidimension and use of for in for. But now, we have a struct that allows instead of types Wappers, type Objects. The main strucut is Map plus List (with  type object specifc) and Object. See the line twenty four.

---
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/SetTest01.java
@@ -0,0 +1,24 @@
+package academy.devdojo.maratonajava.javacore.Ycolecoes.test;
+
+import academy.devdojo.maratonajava.javacore.Ycolecoes.dominio.Manga;
+
+import java.util.*;
+
+public class SetTest01 {
+    public static void main(String[] args) {
+        Set<Manga> mangas = new LinkedHashSet<>();
+        mangas.add(new Manga(5L,"Hellsing Ultimate",19.9,0));
+        mangas.add(new Manga(1L,"Berserk",9.5,5));
+        mangas.add(new Manga(4L,"Pokemon",3.2,0));
+        mangas.add(new Manga(3L,"Attack on titan",11.20,2));
+        mangas.add(new Manga(2L,"Dragon ball 2",2.99,0));
+        mangas.add(new Manga(2L,"Dragon ball 2",2.99,0));
+
+        mangas.getClass();
+
+        for(Manga manga : mangas){
+            System.out.println(manga);
+        }
+
+    }
+}
